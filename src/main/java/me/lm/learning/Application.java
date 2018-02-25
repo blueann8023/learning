2da8be6932c6427c44554c5da54ab069989f019f
ann <408355459@qq.com>
2018-02-25 11:21:15 +0800

    localdate

--- src/main/java/me/lm/learning/Application.java
+++ src/main/java/me/lm/learning/Application.java
@@ -1,7 +1,10 @@
 package me.lm.learning;
 
+import java.time.LocalDate;
+
 public class Application {
     public static void main(String[] args) {
         System.out.print(1);
+        LocalDate localDate = LocalDate.now();
     }
 }
