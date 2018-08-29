4752df686a3853a659fca85b974e42f490a82f37
blue_ann <408355459@qq.com>
2018-08-29 14:36:21 +0800

    item 5

---
+++ src/main/java/me/lm/learning/effective/createordestroyobject/item5/Lexicon.java
@@ -0,0 +1,17 @@
+package me.lm.learning.effective.createordestroyobject.item5;
+
+/**
+ * Created by blue_ann on 29/08/2018.
+ */
+public class Lexicon {
+
+    private String language;
+
+    public Lexicon(String language) {
+        this.language = language;
+    }
+
+    public boolean constains(String word) {
+        return true;
+    }
+}
